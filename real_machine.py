cc53b7e8293ecc14441fcd457a929915a7e0fd84
zhemepatis <rink.gab@gmail.com>
2025-04-07 17:34:34 +0300

    finally something works/usr/bin/python

--- real_machine.py
+++ real_machine.py
@@ -1,4 +1,5 @@
 from virtual_machine import VirtualMachine
+from utils.interface import Interface
 
 class RealMachine:
     def __init__(self, cpu, memory, pagination, channel_device):
@@ -8,6 +9,7 @@ class RealMachine:
         self.pagination = pagination
         self.channel_device = channel_device
         # other
+        self.interface = Interface()
         self.vm_list = []
 
     def create_vm(self):
@@ -44,4 +46,59 @@ class RealMachine:
         pass
 
     def run(self):
-        self.create_vm()
+        while True:
+            self.interface.main_menu()
+
+            # if choice == 1:
+            #     print("\n--- Creating VM and testing memory allocation ---")
+            #     rm.create_vm()
+
+            #     vm = rm.vm_list[0]
+            #     print("Select execution mode:")
+            #     print("1. Run program automatically")
+            #     print("2. Run program step-by-step")
+            #     execution_mode = input("Choose an option: ")
+
+            #     if execution_mode == "1":
+            #         vm.exec()
+            #     elif execution_mode == "2":
+            #         step_by_step_menu(vm)
+
+            # elif choice == 2:
+            #     current_mode = "Supervisor" if cpu.mode == 0 else "User"
+            #     print(f"Current operation mode: {current_mode}")
+            #     new_mode = input("Enter new mode (Supervisor/User): ")
+            #     cpu.mode = 0 if new_mode.lower() == "supervisor" else 1
+
+            # elif choice == 3:
+            #     print("Exiting system. Goodbye!")
+            #     break
+
+            # else:
+            #     print("Invalid choice. Try again!")
+
+    def handle_main_menu(self, choice):
+        if choice == 1:
+            return
+        
+        if choice == 2:
+            return
+        
+        if choice == 3:
+            return
+        
+    def handle_step_by_step_menu(self, choice):
+        if choice == 1:
+            return
+        
+        if choice == 2:
+            return
+        
+        if choice == 3:
+            return
+        
+        if choice == 4:
+            return
+        
+        if choice == 5:
+            return
